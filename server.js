880d662a767e806d4bca3b012fa7ed230924d0d5
Nandeep Shah <nandeep.shah@gmail.com>
2023-02-25 15:23:31 -0500

    end of lectue 47

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require('./app');
 
-const [PORT, HOST] = [3300, '127.0.0.1'];
+// console.log(app.get('env'));
 
+const [PORT, HOST] = [3300, '127.0.0.1'];
+console.log(process.env.NODE_ENV);
 app.listen(PORT, HOST, () => {
 	console.log('Server Started');
 });
