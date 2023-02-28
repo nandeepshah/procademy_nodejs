490e534db7a7a9c8abdafc45c17982da000c6190
Nandeep Shah <nandeep.shah@gmail.com>
2023-02-27 21:02:10 -0500

    end of lecture 65

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+mongoose
+	.connect(process.env.CONN_STR, {
+		useNewUrlParser: true,
+	})
+	.then(conn => {
+		console.log('DB Connection Success');
+	})
+	.catch(err => {
+		console.log('Something went wrong');
+	});
+
+const movieSchema = new mongoose.Schema({
+	name: {
+		type: Number,
+		required: true,
+		unique: true,
+	},
+	description: String,
+	duration: {
+		type: Number,
+		required: true,
+	},
+	ratings: {
+		type: Number,
+		default: 1.0,
+	},
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
 
 const PORT = process.env.PORT || 3300;
 const HOST = '127.0.0.1';
