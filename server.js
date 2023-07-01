9a9b9f1360e1e5aed478e1e16ba2d87d6a9758f7
Nandeep Shah <nandeep.shah@gmail.com>
2023-07-01 19:01:13 -0400

    adding comments to server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
+// Mongoose is an ODM driver for MongoDB
 const mongoose = require('mongoose');
+// dotenv is an package for reading enviorenment variables in node
 const dotenv = require('dotenv');
+// provide the path to dotenv where the config file is stored.
 dotenv.config({ path: './config.env' });
+// get all the configurations for the server
 const app = require('./app');
 
+// define port and host variables
 const PORT = process.env.PORT || 3300;
-const HOST = '127.0.0.1';
+const HOST = process.env.HOST;
 
+// start the server
 app.listen(PORT, HOST, () => {
 	console.log('Server Started');
 });
 
+// connect to MongoDB
 mongoose
 	.connect(process.env.CONN_STR, {
 		useNewUrlParser: true,
